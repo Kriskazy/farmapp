f49e776f890dbd3ca8177a6fced27b3321335de0
Kazeem Folorunso <donkris93@gmail.com>
2025-09-18 21:13:47 +0100

    fixed login issu

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,3 @@
-export const API_URL = process.env.REACT_APP_BASE_URL;
-console.log('API_URL is:', API_URL); // Debug log
+let rawUrl = process.env.REACT_APP_BASE_URL || '';
+export const API_URL = rawUrl.replace(/\/+$/, ''); // remove trailing slashes
+console.log('Sanitized API_URL:', API_URL);
